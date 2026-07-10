4b38998aff9090a7cf413e57602e9d4d2d30ddf2
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-07-10 17:41:22 +0700

    Update WhatsApp owner and phone number in config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
   botName: 'SkyLight',
   version: '1.0.0',
-  owner: ['628xxx'], // Ganti dengan nomor WhatsApp owner Anda (tanpa @s.whatsapp.net)
+  owner: ['6285135044757'], // Ganti dengan nomor WhatsApp owner Anda (tanpa @s.whatsapp.net)
   prefix: '.',
   pairing: {
     enabled: true, // Ubah ke false jika ingin menggunakan QR Code login
-    phoneNumber: '628xxx' // Nomor WhatsApp yang akan dipasangkan jika pairing.enabled = true
+    phoneNumber: '6288219426032' // Nomor WhatsApp yang akan dipasangkan jika pairing.enabled = true
   },
   timezone: 'Asia/Jakarta',
   theme: {
