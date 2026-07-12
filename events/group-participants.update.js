3dc355c044c8d9663e320d064d0b0f5df0726702
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-07-12 16:39:52 +0700

    Enhance welcome and goodbye messages for participants

--- events/group-participants.update.js
+++ events/group-participants.update.js
@@ -1,32 +1,35 @@
 import database from '../lib/database.js';
-import { decodeJid } from '../lib/utils.js'; // Import dekoder JID
+import { decodeJid } from '../lib/utils.js';
 
 export default {
   name: 'group-participants.update',
   async execute(sock, update) {
     try {
       const { id, participants, action } = update;
-      
-      // Pastikan fitur welcome aktif di database grup ini
-      const isWelcomeEnabled = database.isPluginEnabled(id, 'welcome');
-      if (!isWelcomeEnabled) return;
 
       for (const num of participants) {
-        const cleanNum = decodeJid(num); // Dekode JID agar terbebas dari kode perangkat (:X)
+        const cleanNum = decodeJid(num);
         const username = cleanNum.split('@')[0];
-        
-        let text = '';
-        if (action === 'add') {
-          text = `*👋 HALO & SELAMAT DATANG! 👋*\n\nSelamat datang @${username} di grup ini!\nSemoga betah dan silakan perkenalkan diri Anda dengan baik.`;
-        } else if (action === 'remove') {
-          text = `*😢 GOODBYE / SELAMAT TINGGAL! 😢*\n\n@${username} telah meninggalkan grup. Terima kasih atas kebersamaannya selama ini.`;
-        }
 
-        if (text) {
-          await sock.sendMessage(id, {
-            text,
-            mentions: [cleanNum]
-          });
+        // Sapaan Anggota Baru (Welcome)
+        if (action === 'add') {
+          const isWelcomeEnabled = database.isPluginEnabled(id, 'welcome');
+          if (isWelcomeEnabled) {
+            await sock.sendMessage(id, {
+              text: `*👋 HALO & SELAMAT DATANG! 👋*\n\nSelamat datang @${username} di grup ini!\nSemoga betah dan silakan perkenalkan diri Anda dengan baik.`,
+              mentions: [cleanNum]
+            });
+          }
+        } 
+        // Sapaan Anggota Keluar (Goodbye)
+        else if (action === 'remove') {
+          const isGoodbyeEnabled = database.isPluginEnabled(id, 'goodbye');
+          if (isGoodbyeEnabled) {
+            await sock.sendMessage(id, {
+              text: `*😢 GOODBYE / SELAMAT TINGGAL! 😢*\n\n@${username} telah meninggalkan grup. Terima kasih atas kebersamaannya selama ini.`,
+              mentions: [cleanNum]
+            });
+          }
         }
       }
     } catch (err) {
